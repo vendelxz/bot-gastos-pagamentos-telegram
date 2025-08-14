768f7e3ba63dd53187a3151d86edfe1b0bf5c91e
Wendel <jwend3l@gmail.com>
2025-08-13 22:54:50 -0300

    bug: duas funções /start no bot.py

--- controle-financeiro/bot.py
+++ controle-financeiro/bot.py
@@ -31,9 +31,6 @@ if os.getenv("RUN_SERVER") == "1":
 # Bot Telegram
 app = ApplicationBuilder().token(TOKEN).build()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot ativo!")
-
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
